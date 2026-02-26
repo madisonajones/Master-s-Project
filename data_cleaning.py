f95ff409c155543999e1b474fe299ab573aa46bb
Madison Jones <madisonannejoness@gmail.com>
2026-02-25 23:31:06 -0500

    Week 6 file created, saving EDA_2, updates to data cleaning

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,6 +5,8 @@ def cleaning_data(df):
     Returns cleaned dataset.'''
     # clean_df = df.dropna(axis ='rows')
     clean_df = df[df['Year'].isin([2019,2022])]
+
+    # rename columns for easier use in code
     clean_df = clean_df.rename({'DBN': 'dbn',
                                 'School Name': 'school_name',
                                 'Number Tested': "number_tested",
@@ -25,6 +27,51 @@ def cleaning_data(df):
 def change_variable_type(df,columns):
     ''' This function converts numerical columns to int64. Initially, percentages and numbers 
     are objects.'''
+    # convert num columns to numeric, coerce errors to NaN
     for col in columns:
         df[col] = pd.to_numeric(df[col], errors = 'coerce')
     return df
+
+
+
+def create_school_level_data(df):
+    ''' Filters data for school level information for all grades and all students. Returns new dataframe'''
+
+    # filtering for school level data, all grades, all students
+    df = df[
+        (df['Report Category'].isin(['School'])) &
+        (df['Grade'] == 'All Grades') &
+        (df['Student Category'] == 'All Students')
+    ]
+    return df
+
+
+def merge_data_and_poverty(schooldata, povertydata):
+    ''' Cleans poverty data, adjusts column names, merges with school level data. Returns merged dataframe.'''
+
+    # clean poverty data, filter for 2018-19 school year, adjust column names
+    cols_to_keep = ['DBN','School Name','Year','% Poverty']
+    povertydata_filtered = povertydata[
+        (povertydata['Year'] == '2018-19')][cols_to_keep]
+    
+    # fix 0.95 value in poverty data
+    povertydata_filtered['% Poverty'] = povertydata_filtered['% Poverty'].replace({'Above 95%':'0.95'})
+
+    # rename for merge in school data
+    schooldata= schooldata.rename(columns = {'Geographic Subdivision':'DBN'})
+
+    # merge school & poverty data on DBN (school identifier)
+    merged = schooldata.merge(povertydata_filtered[['DBN', '% Poverty']],  # keep only needed columns
+                              on='DBN',
+                              how='left')
+    
+    return merged
+
+
+def categorize_poverty(row):
+    nyc_poverty_percent = 0.74
+    if row['% Poverty'] >= nyc_poverty_percent:
+        return '1'
+    else:
+        return '0'
+    
